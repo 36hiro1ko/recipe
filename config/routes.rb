c52dce616b939d14b60f1f2656d6552ad8ead0ba
a36hiro1ko <starlilycat22272@nifty.com>
2015-08-29 08:16:09 +0000

    create session helper

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
 
   root to: 'static_pages#home'
   get 'signup', to: 'users#new' #signupという名前でアクセスできるように
+  get 'login', to:  'sessions#new'
+  post   'login',   to: 'sessions#create'
+  delete 'logout',  to: 'sessions#destroy'
   
   resources :users
-
+  resources :sessions, only:[:new, :create, :destroy]
 end
