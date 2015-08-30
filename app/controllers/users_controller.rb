faf8d5aded81429c905009357908f0c2c862d54c
a36hiro1ko <starlilycat22272@nifty.com>
2015-08-30 06:10:59 +0000

    add function for user page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,11 @@
 class UsersController < ApplicationController
   def show
-    @user= User.find(params[:id]);
+    if logged_in?
+      @user= User.find(params[:id]);
+    else
+      flash[:warning] = "このページを閲覧するためにはログインが必要です"
+      redirect_to login_path
+    end
   end
   
   def new
