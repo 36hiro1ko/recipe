e0f5a44f27f792c9dde52602c399de7cbb854937
a36hiro1ko <starlilycat22272@nifty.com>
2015-09-01 07:09:10 +0000

    add posting microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,8 @@ class UsersController < ApplicationController
   
   def show
     if logged_in?
-      @user= User.find(params[:id]);
+      @user= User.find(params[:id])
+      @microposts = @user.microposts
     else
       flash[:warning] = "このページを閲覧するにはログインが必要です"
       redirect_to login_path
