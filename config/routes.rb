1d701ab5a3210a404df0b750545f9f6fb1688b91
a36hiro1ko <starlilycat22272@nifty.com>
2015-09-03 04:29:18 +0000

    add 404 error page

--- config/routes.rb
+++ config/routes.rb
@@ -13,4 +13,8 @@ Rails.application.routes.draw do
   resources :sessions, only:[:new, :create, :destroy]
   resources :microposts
   resources :relationships, only: [:create, :destroy]
+  
+  
+  
+  get '*path', to: 'application#render_404'
 end
